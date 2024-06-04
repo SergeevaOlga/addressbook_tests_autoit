1b1c2fbb678ad9b4882277de7a9343f0e89d8d83
SergeevaOlga <freyaspb@mail.ru>
2024-06-04 20:26:48 +0300

    Добавьте файлы проекта.

---
+++ addressbook_tests_autoit/appmanager/ApplicationManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoItX3Lib;
+
+namespace addressbook_tests_autoit
+{
+    public class ApplicationManager
+    {
+        public static string WINTITLE = "Free Address Book";
+        private AutoItX3 aux;
+        private GroupHelper groupHelper;
+
+
+        public ApplicationManager()
+        {
+            aux = new AutoItX3();
+            aux.Run(@"C:\Users\freya\source\repos\FreeAddressBookPortable\AddressBook.exe", "", aux.SW_SHOW);
+            aux.WinActivate(WINTITLE);
+            aux.WinWait(WINTITLE);
+            aux.WinWaitActive(WINTITLE);
+
+            groupHelper = new GroupHelper(this);
+        }
+
+        public void Stop()
+        {
+            aux.ControlClick(WINTITLE, "$Exit", "WindowsForms10.BUTTON.app.0.1114f81");
+            aux.WinClose(WINTITLE);
+        }
+
+        public AutoItX3 Aux
+        {
+            get 
+            {
+                return aux;
+            }
+        }
+
+
+        public GroupHelper Groups {
+            get { return groupHelper; }
+        }
+    }
+
+}
